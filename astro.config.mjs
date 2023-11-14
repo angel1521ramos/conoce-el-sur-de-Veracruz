e59485a08bfb328c767e8a7d6ebeda12c16dd56b
angel <angel1521ramos@gmail.com>
2023-11-14 00:23:12 -0600

    segundo commit deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://angel1521ramos.github.io',
+  base: '/conoce-el-sur-de-Veracruz',
 });
